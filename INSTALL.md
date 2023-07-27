faf273ae0e94b487f7ff87e11167c0584db5ec56
marat <zm>
2023-07-27 11:05:11 +0300

    update packages

---
+++ INSTALL.md
@@ -0,0 +1,129 @@
+# Установка CryptoPro CSP
+
+## Установка из дистрибутива
+
+Дистрибутив находится в директории `cryptopro-pkg` данного проекта. Для установки дистрибутива необходимо
+извлечь содержимое архива и выполнить скрипт `install.sh` от имени суперпользователя:
+
+```bash
+cd cryptopro-pkg
+tar xzvf linux-amd64_deb.tgz
+cd linux-amd64_deb
+sudo bash ./install.sh lsb-cprocsp-devel
+
+# Также необходимо установить cades-плагин
+cd ..
+sudo dpkg -i ./cprocsp-pki-cades_2.0.0-1_amd64.deb
+```
+
+## Настройка
+### Провайдер по умолчанию
+После установки имеет смысл сразу выбрать `Crypto-Pro GOST R 34.10-2012 KC1 CSP` в качестве провайдера по умолчанию:
+
+```bash
+sudo /opt/cprocsp/sbin/amd64/cpconfig -defprov -setdef -provtype 80 -provname 'Crypto-Pro GOST R 34.10-2012 KC1 CSP'
+```
+
+### Настройка ГСЧ
+Чтобы обеспечить автономность выполнения тестов можно настроить криптопро на использование внешней гаммы вместо
+биалогического ГСЧ. Гамму можно клонировать из образца, расположенного в `cryptopro-pkg/kis_1` (⚠️ данный сценарий
+подходит для тестов во временном тестовом окружении, в других окружениях должен использоваться сертифицированный
+физический ГСЧ):
+
+```bash
+# Директории для будущей гаммы
+mkdir -p db1
+mkdir -p db2
+
+# Клонируем гамму много раз
+for x in {1..100}
+    do cat path/to/kis_1 >> db1/kis_1
+done
+
+# Клонируем результат еще много раз
+for x in {1..1000}
+    do cat db1/kis_1 >> db2/kis_1
+done
+
+# Используем самый большой результат
+cp db2/kis_1 db1/kis_1
+
+#
+# Настраиваем КриптоПро на использование нашей гаммы.
+#
+
+# 1. Создаем ГСЧ
+sudo /opt/cprocsp/sbin/amd64/cpconfig -hardware rndm -add cpsd -name 'cpsd rng' -level 3
+
+# 2. Настраиваем первую гамму
+sudo /opt/cprocsp/sbin/amd64/cpconfig \
+    -hardware rndm -configure cpsd \
+    -add string \
+    `# Это внутренний путь криптопровайдера и он должен быть именно таким`
+    /db1/kis_1 /path/to/db1/kis_1 \
+    `# Это гамма, которую мы сгенерировали ниже`
+    path/to/db1/kis_1
+
+# 3. Настраиваем вторую гамму
+sudo /opt/cprocsp/sbin/amd64/cpconfig \
+    -hardware rndm -configure cpsd \
+    -add string \
+    `# Это внутренний путь криптопровайдера и он должен быть именно таким` \
+    /db2/kis_1 \
+    `# Это гамма, которую мы сгенерировали ниже` \
+    /path/to/db2/kis_1
+
+# Проверяем работу гаммы (первый запуск может вывести ошибку).
+# Результатом должна быть успешная генерация ключа без запросов на ввод символов с клавиатуры
+/opt/cprocsp/bin/amd64/csptest -keyset -newkeyset -machinekeyset -password 123456 -hard_rng -container 'HDIMAGE\\dummy'
+```
+
+### Логирование
+При разработе может оказаться полезно логирование, встроенное в различные модули КриптоПро. Включить полное логирование
+для модулей можно следующими командами:
+
+```bash
+sudo /opt/cprocsp/sbin/amd64/cpconfig -loglevel ocsp -mask 0xF
+sudo /opt/cprocsp/sbin/amd64/cpconfig -loglevel tsp -mask 0xF
+sudo /opt/cprocsp/sbin/amd64/cpconfig -loglevel cades -mask 0xF
+sudo /opt/cprocsp/sbin/amd64/cpconfig -loglevel cpcsp -mask 0xF
+sudo /opt/cprocsp/sbin/amd64/cpconfig -loglevel capi10 -mask 0xF
+sudo /opt/cprocsp/sbin/amd64/cpconfig -loglevel cprdr -mask 0xF
+sudo /opt/cprocsp/sbin/amd64/cpconfig -loglevel cpext -mask 0xF
+sudo /opt/cprocsp/sbin/amd64/cpconfig -loglevel capi20 -mask 0xF
+sudo /opt/cprocsp/sbin/amd64/cpconfig -loglevel capilite -mask 0xF
+```
+
+После настройки логи будут записываться в системный лог, начать просмотр которого можно командой:
+
+```bash
+tail -f /var/log/syslog
+```
+
+### Настройка окружения и пути к заголовочным файлам
+Для сборки проектов, использующих КриптоПро CSP необходимо настроить окружение:
+
+```bash
+# Необходимо добавить путь к динамическим библиотекам КриптоПро в текущее окружение
+export LD_LIBRARY_PATH=/opt/cprocsp/lib/amd64
+```
+
+Также при компиляции необходимо указывать следующие пути к заголовочным файлам (если применимо в вашем случае):
+
+1. `/opt/cprocsp/include`
+1. `/opt/cprocsp/include/cpcsp`
+1. `/opt/cprocsp/include/asn1c/rtsrc`
+1. `/opt/cprocsp/include/asn1data`
+1. `/opt/cprocsp/include/pki`
+
+Например в данных биндингах заголовочные файлы используются для автоматической генерации определений функций С-интерфейса КриптоПро CSP. Ключевые заголовки КриптоПро CSP импортируются в файл [`cryptopro_wrapper.h`](exonum-crypto-gost/headers/cryptopro_wrapper.h), который передается в [rust-bindgen](https://github.com/rust-lang/rust-bindgen) для генерации биндингов. Настройка rust-bindgen производится в файле [`gen_cryptopro.rs`](exonum-crypto-gost/gen_cryptopro.rs).
+
+### Установка необходимых сертификатов
+Для успешного прохождения всех юнит-тестов, может потребоваться установка корневого сертификата тестового центра КриптоПро:
+```bash
+/opt/cprocsp/bin/amd64/certmgr -install -store root -f ~/cryptopro-pkg/test_root_ca.cer
+```
+Также понадобится получить сертификат из тестового центра КриптоПро для создания подписи CAdES-BES:
+```bash
+/opt/cprocsp/bin/amd64/cryptcp -createcert -rdn 'e=email@test.ru,cn="CryptoPro Test Cert",c=Russia,l="City",o="Test"' -cont '\\.\HDIMAGE\test_cpro'
+```
--- install.sh
+++ install.sh
@@ -1,49 +1,146 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-rm -rf linux-amd64_deb 
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
 
-tar zxvf ./linux-amd64_deb.tgz
-bash ./linux-amd64_deb/uninstall.sh
-rm -f /var/opt/cprocsp/tmp/.reqistry_lock
-rm -rf /opt/cprocsp
-rm -rf /etc/opt/cprocsp
-rm -rf /var/opt/cprocsp
+CPCONFIG=/opt/cprocsp/sbin/amd64/cpconfig
+CSPTEST=/opt/cprocsp/bin/amd64/csptest
+KIS_1=/var/opt/cprocsp/dsrf/db1/kis_1
+KIS_2=/var/opt/cprocsp/dsrf/db2/kis_1
 
-pushd linux-amd64_deb
-echo Install cryptopro csp
+KIS_SAMPLE="W+bji05lOwHfTXYJcd5SpS4+30j5sgneR5Zyl3rI//1rnR+veFeiCvM66Yobd9dTj0K66M1s2p1G/NpG0sIxV3Pj2C+QNyIy"
 
-dpkg -i cprocsp-curl* lsb-cprocsp-base* lsb-cprocsp-capilite* lsb-cprocsp-kc1* lsb-cprocsp-rdr-64*
-dpkg -i cprocsp-rdr-rutoken* cprocsp-rdr-pcsc* 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Override paths
+  CPCONFIG=/opt/cprocsp/sbin/cpconfig
+  CSPTEST=/opt/cprocsp/bin/csptest
+fi
 
-dpkg -i lsb-cprocsp-ca-certs*
-dpkg -i lsb-cprocsp-devel*
+function install() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Installing.."
+    hdiutil attach ru.cryptopro.csp-4.0.9963.dmg
+    installer -package /Volumes/ru.cryptopro.csp-4.0.9963/ru.cryptopro.csp-4.0.9963.mpkg -target /
+    hdiutil detach /Volumes/ru.cryptopro.csp-4.0.9963
+  else
+    apt-get update
+    apt-get install -y clang llvm build-essential git language-pack-ru libssl-dev pkg-config
+    (tar xzvf linux-amd64_deb.tgz && cd linux-amd64_deb && bash ./install.sh lsb-cprocsp-devel)
+    rm -rf linux-amd64_deb
+    $CPCONFIG -defprov -setdef -provtype 80 -provname 'Crypto-Pro GOST R 34.10-2012 KC1 CSP'
+  fi
+}
+
+function install_cades() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Installing cades.."
+    hdiutil attach cprocsp-pki-2.0.0.dmg
+    installer -package /Volumes/cprocsp-pki-2.0.0/cprocsp-pki-2.0.0.mpkg -target /
+    hdiutil detach /Volumes/cprocsp-pki-2.0.0
+  else
+    dpkg -i cprocsp-pki-cades_2.0.0-1_amd64.deb
+  fi
+}
+
+function uninstall() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Uninstalling.."
+    hdiutil attach ru.cryptopro.csp-4.0.9963.dmg
+    /Volumes/ru.cryptopro.csp-4.0.9963/uninstall_csp.app/Contents/MacOS/applet
+    hdiutil detach /Volumes/ru.cryptopro.csp-4.0.9963
+  else
+    (tar xzvf linux-amd64_deb.tgz && cd linux-amd64_deb && bash ./uninstall.sh)
+    rm -rf linux-amd64_deb
+  fi
+}
+
+function check_and_fix_license() {
+  if [[ $($CPCONFIG -license -view 2>&1) == *"Error code"* ]]; then
+    echo "Fixing license.."
+    rm /etc/opt/cprocsp/license.ini
+    uninstall
+    install
+  fi
+
+  if [[ $($CPCONFIG -license -view 2>&1) == *"Error code"* ]]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+function check_and_fix_rng() {
+  if [[ $($CSPTEST -keyset -newkeyset -password 123456 -hard_rng -container 'HDIMAGE\\dummy68' -silent 2>&1) == *"80090022"* ]]; then
+    echo "Fixing RNG.."
+    $CPCONFIG -hardware rndm -add cpsd -name 'cpsd rng' -level 3
+
+    for x in {1..100}; do
+      echo $KIS_SAMPLE | base64 --decode >>$KIS_1
+    done
+
+    for x in {1..1000}; do
+      cat $KIS_1 >>$KIS_2
+    done
+
+    cp $KIS_2 $KIS_1
+
+    chmod 666 $KIS_1
+    chmod 666 $KIS_2
+  else
+    $CSPTEST -keyset -deletekeyset -password 123456 -hard_rng -container 'HDIMAGE\\dummy68' -silent 2>&1 >/dev/null
+  fi
+
+  if [[ $($CSPTEST -keyset -newkeyset -password 123456 -hard_rng -container 'HDIMAGE\\dummy68' -silent 2>&1) == *"80090022"* ]]; then
+    return 1
+  else
+    $CSPTEST -keyset -deletekeyset -password 123456 -hard_rng -container 'HDIMAGE\\dummy68' -silent 2>&1 >/dev/null
+    return 0
+  fi
+}
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
+if [[ ! -f $CPCONFIG || ! -f $CSPTEST ]]; then
+  # CryptoPro is not istalled
+  install
+fi
+
+install_cades
+
+if ! check_and_fix_license; then
+  echo "FATAL: Can't fix license"
+  exit 1
+fi
+
+if ! check_and_fix_rng; then
+  echo "FATAL: Can't fix RNG"
+  exit 1
+fi
+
+$CPCONFIG -loglevel ocsp -mask 0xF
+$CPCONFIG -loglevel tsp -mask 0xF
+$CPCONFIG -loglevel cades -mask 0xF
+$CPCONFIG -loglevel cpcsp -mask 0xF
+$CPCONFIG -loglevel capi10 -mask 0xF
+$CPCONFIG -loglevel cprdr -mask 0xF
+$CPCONFIG -loglevel cpext -mask 0xF
+$CPCONFIG -loglevel capi20 -mask 0xF
+$CPCONFIG -loglevel capilite -mask 0xF
 
 
-echo Install cades plugins
-dpkg -i cprocsp-pki-cades*
-dpkg -i cprocsp-pki-plugin*
-
-popd
-
-# rm -rf linux-amd64_deb 
-
-/opt/cprocsp/sbin/amd64/cpconfig -license -view 
-
-echo Create container brc
-
-# /opt/cprocsp/sbin/amd64/csptest -keyset -provtype 80 -newkeyset -cont '\\.\HDIMAGE\bcr' 
-echo Show container brc
-/opt/cprocsp/bin/amd64/csptest -keyset -enum_cont -fqcn -veryfic | iconv -f cp1251 
-
-# create container for delete
-/opt/cprocsp/bin/amd64/csptest -keyset -newkeyset -cont 'for delete' -pass 1234
-# show container for delete
-/opt/cprocsp/bin/amd64/csptest -keyset -enum_cont -fqcn -verifyc
-# own self signed certificates
-/opt/cprocsp/bin/amd64/csptest -keyset -makecert -container 'for delete'
-# look for self signed certificates
-/opt/cprocsp/bin/amd64/certmgr -list -cont 'for delete'
-# sudo bash ./install.sh
-
-# delete container
-/opt/cprocsp/bin/amd64/csptest -keyset -deletekeyset -cont '\\.\HDIMAGE\test'
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  LIB_PATH=/opt/cprocsp/lib
+  CADES_PATH=/Applications/CryptoPro_ECP.app/Contents/MacOS/lib
+  echo -e "\n${GREEN}Done!${NC} Do not forget to properly setup the environment:"
+  echo -e "  ${YELLOW}export DYLD_LIBRARY_PATH=\"${LIB_PATH}:${CADES_PATH}:\$DYLD_LIBRARY_PATH\"${NC}"
+else
+  echo -e "\n${GREEN}Done!${NC} Please relogin to setup the environment."
+  rm -f /etc/ld.so.conf.d/cpcsp.conf
+  echo "/opt/cprocsp/lib/amd64" >> /etc/ld.so.conf.d/cpcsp.conf
+  exit 0
+fi
